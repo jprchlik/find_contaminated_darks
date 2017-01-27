0f6e55e4eac01a7e37a3d235cee80bf3b56e5c90
jprchlik <japrchlik@gmail.com>
2017-01-27 13:50:51 -0500

    Added find_dark_runs.py which queries google calendar for dark runs and returns the date

---
+++ find_dark_runs.py
@@ -0,0 +1,96 @@
+####init api to parse Google calendar for calibration-as-run for Calib3: darks
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+
+    Creates a Google Calendar API service object and outputs a list of the 
+    Dark runs on IRIS calibration-as-run calendar.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+#    calendar_list_entry = service.calendarList().get(calendarId='calendarId').execute()
+#    print(calendar_list_entry['summary'])
+    
+#GET LIST OF CALENDARS 
+#    page_token = None
+#    while True:
+#      calendar_list = service.calendarList().list(pageToken=page_token).execute()
+#      for calendar_list_entry in calendar_list['items']:
+#        print(calendar_list_entry)
+#      page_token = calendar_list.get('nextPageToken')
+#      if not page_token:
+#        break
+
+    span = 31
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    onmonth = (datetime.datetime.utcnow()-datetime.timedelta(days=span)).isoformat()+'Z'
+    print('Getting days with darks')
+    eventsResult = service.events().list(
+        calendarId='27f4lqaadrbrp1nueps13qq2n0@group.calendar.google.com', timeMin=onmonth,timeMax=now, singleEvents=True, #IRIS calibration-as-run
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+
+    darks = 'Calib 3: Dark'.upper().replace(' ','').replace(':','')
+
+    if not events:
+        print('No darks found in last {0:3d}.'.format(span))
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        eventstring = event['summary'].upper().replace(' ','').replace(':','')
+        if eventstring == darks:
+            print(start, event['summary'])
+
+
+if __name__ == '__main__':
+    main()
