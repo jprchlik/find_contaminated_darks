dd3a329a5165d340579be465c1826d44a68303d3
jprchlik <japrchlik@gmail.com>
2017-02-03 17:21:32 -0500

    Now get_dark_files querys jsoc, however, nothing returns currently

--- get_dark_files.py
+++ get_dark_files.py
@@ -8,13 +8,14 @@ import requests
 class dark_times:
 
 
-    def __init__(self,time,irisweb='http://iris.lmsal.com/health-safety/timeline/iris_tim_archive/2017/01/11/IRIS_science_timeline_{0}.V{1:2d}.txt'):
+    def __init__(self,time,irisweb='http://iris.lmsal.com/health-safety/timeline/iris_tim_archive/IRIS_science_timeline_{0}.V{1:2d}.txt'):
 
 
 #web page location of IRIS timeline
-        self.irisweb = irisweb
+        self.irisweb = irisweb.replace('IRIS',time+'/IRIS')
         self.otime = dt.datetime.strptime(time,'%Y/%m/%d')
         self.stime = self.otime.strftime('%Y%m%d')
+        self.obsid = 'OBSID=4202000003'
         self.obsid = 'OBSID=4202000003'
         
     def request_files(self):
@@ -62,8 +63,44 @@ class dark_times:
             
 #set up JSOC query for darks
     def dark_query(self):
+        from sunpy.net import jsoc
+        client = jsoc.JSOCClient()
         fmt = '%Y.%m.%d_%H:%M'
-        self.qstr = 'iris.lev1[{0}_TAI-{1}_TAI][][? IMG_TYPE ~ "DARK" ?]'.format(self.sta_dark_dt.strftime(fmt),self.end_dark_dt.strftime(fmt)) 
+#        self.qstr = 'iris.lev1[{0}_TAI-{1}_TAI][][? IMG_TYPE ~ "DARK" ?]'.format(self.sta_dark_dt.strftime(fmt),self.end_dark_dt.strftime(fmt)) 
+        #setup string to pass write to sswidl for download
+        fmt = '%Y-%m-%dT%H:%M:%S'
+        self.response = client.query(jsoc.Time(self.sta_dark_dt.strftime(fmt),self.end_dark_dt.strftime(fmt)),jsoc.Series('iris.lev1'),
+                                jsoc.Notify('jakub.prchlik@cfa.harvard.edu'))
+  
+        self.get_darks(client)
+
+    def get_darks(self,client):
+        
+        import time
+        wait = True
+
+        request = client.request_data(self.response)
+        waittime = 60.*5. #five minute wait to check on data completion
+        time.sleep(waittime) #
+
+        while wait:
+            stat = client.check_request(request)
+            if stat == 1:
+                temp.sleep(waittime)
+            elif stat == 0:
+                wait = False
+            elif stat > 1:
+                break #jump out of loop if you get an error
+        #make the download directory
+        bdir = '/data/alisdair/IRIS_LEVEL_1_DARKS/{0}/simpleB/'.format(self.otime.strftime('%Y/%m'))
+        try:
+            os.makedirs(bdir)
+        except IOError:
+            time.sleep(1)
+ 
+        #download the data
+        res = client.get_request(request,path=bdir,progress=True)
+        res.wait()
 
 #run to completion
 
