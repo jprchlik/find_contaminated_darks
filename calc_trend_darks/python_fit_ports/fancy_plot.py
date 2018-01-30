07fcea4a153c8c6f54fc1ea5c3e06f7bbe646e27
jprchlik <japrchlik@gmail.com>
2018-01-30 13:56:03 -0500

    Added fancy_plot modules

---
+++ calc_trend_darks/python_fit_ports/fancy_plot.py
@@ -0,0 +1,16 @@
+
+#add tick marks to plots
+def fancy_plot(ax):
+    #Turn minor ticks on
+    ax.minorticks_on()
+    ax.yaxis.set_ticks_position('both')
+    ax.xaxis.set_ticks_position('both')
+    #set the width of the ticks
+    ax.tick_params(which='both',width=1)
+    #set the length of the major ticks
+    ax.tick_params(which='major',length=7)
+    #set length of the minor ticks
+    ax.tick_params(which='minor',length=3,direction='in')
+    ax.tick_params(direction='in')
+    ax.grid(color='gray',linestyle='-.',linewidth=2)
+    return ax
