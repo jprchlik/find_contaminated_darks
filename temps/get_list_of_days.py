e5b57476a12316813063b688d63a5fefefd03c3e
jprchlik <japrchlik@gmail.com>
2019-01-22 16:42:09 -0500

    Made get_list_of_days.py python3 compatatible

--- temps/get_list_of_days.py
+++ temps/get_list_of_days.py
@@ -1,7 +1,14 @@
 import datetime as dt
 import glob
 import numpy as np
-import urllib2,os
+try:
+    #for python 3.0 or later
+    from urllib.request import urlopen
+except ImportError:
+    #Fall back to python 2 urllib2
+    from urllib2 import urlopen
+
+import os
 from format_temps import format_file
 
 files = glob.glob('../txtout/*txt')
@@ -16,14 +23,14 @@ for i in files:
     utime = np.unique(time)
     for p in utime: 
         for j in np.arange(-2,2):
-            day = dt.datetime.strptime(p,'%Y/%m/%d')+dt.timedelta(days=j)
+            day = dt.datetime.strptime(p.decode('utf-8'),'%Y/%m/%d')+dt.timedelta(days=int(j))
             t = day.strftime('%Y%m%d')
 
             fname= '{0}_iris_temp.txt'.format(t)
             if os.path.isfile(fname):
                 continue
             else:
-               res = urllib2.urlopen('http://www.lmsal.com/~boerner/iris/temps/{0}'.format(fname))
+               res = urlopen('http://www.lmsal.com/~boerner/iris/temps/{0}'.format(fname))
                dat = res.read()
                fo = open(fname,'w')
                fo.write(dat)
